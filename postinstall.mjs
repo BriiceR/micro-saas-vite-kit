8fcc26e2f500047ef2062c114d6f8046bbf7111b
brice <bricerabouin@hotmail.com>
2024-08-06 01:23:40 +0200

    update postinstall

--- postinstall.mjs
+++ postinstall.mjs
@@ -1,47 +1,19 @@
-import fs from 'fs';
-import path from 'path';
-import { execSync } from 'child_process';
-import { promisify } from 'util';
-import { fileURLToPath } from 'url';
+import { execSync } from 'node:child_process';
+import { join } from 'node:path';
+import { existsSync, mkdirSync } from 'node:fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Répertoire où les fichiers doivent être copiés
+const targetDir = join(process.cwd(), 'src');
 
-const exec = promisify(execSync);
-
-async function cloneRepo() {
-  const repoUrl = 'https://github.com/BriiceR/Starter-saas-kit.git';
-  const tempDir = path.join(__dirname, 'temp-repo');
-
-  try {
-    // Cloner le dépôt dans un répertoire temporaire
-    console.log('Clonage du dépôt...');
-    exec(`git clone ${repoUrl} ${tempDir}`, { stdio: 'inherit' });
-
-    // Copier le contenu du répertoire temporaire vers le répertoire courant
-    console.log('Copie des fichiers...');
-    fs.readdirSync(tempDir).forEach(file => {
-      const sourcePath = path.join(tempDir, file);
-      const destPath = path.join(__dirname, file);
-
-      if (fs.lstatSync(sourcePath).isDirectory()) {
-        fs.readdirSync(sourcePath).forEach(subFile => {
-          const subSourcePath = path.join(sourcePath, subFile);
-          const subDestPath = path.join(destPath, subFile);
-          fs.copyFileSync(subSourcePath, subDestPath);
-        });
-      } else {
-        fs.copyFileSync(sourcePath, destPath);
-      }
-    });
-
-    console.log('Nettoyage...');
-    fs.rmdirSync(tempDir, { recursive: true });
-
-    console.log('Le dépôt a été copié avec succès.');
-  } catch (error) {
-    console.error('Erreur lors de la copie du dépôt:', error);
-  }
+// Créer le répertoire cible s'il n'existe pas
+if (!existsSync(targetDir)) {
+  mkdirSync(targetDir, { recursive: true });
 }
 
-cloneRepo();
+// Exécuter la commande pour cloner le dépôt GitHub
+try {
+  execSync('git clone https://github.com/BriiceR/Starter-saas-kit.git src --depth 1', { stdio: 'inherit' });
+  console.log('Le dépôt a été cloné avec succès dans le répertoire "src".');
+} catch (error) {
+  console.error('Erreur lors de la clonage du dépôt :', error.message);
+}
